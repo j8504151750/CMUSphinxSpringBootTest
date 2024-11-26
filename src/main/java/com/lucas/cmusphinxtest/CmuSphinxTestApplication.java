549e606fcc1df0b42c1d483fb1b8b8658c3521cf
lucas <lucas@e-mon.com.tw>
2024-11-26 11:48:54 +0800

    CMUSphinx 初始化

---
+++ src/main/java/com/lucas/cmusphinxtest/CmuSphinxTestApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.cmusphinxtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmuSphinxTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmuSphinxTestApplication.class, args);
+    }
+
+}
